f5e67e186f192b1e00c8641509a6986b9b0329b1
Local User <donotemailme@xganon.com>
2026-02-22 22:23:31 -0600

    Raid menu is working

--- dashboard.php
+++ dashboard.php
@@ -25,14 +25,14 @@ if ($view === '') {
 <html>
 <head>
     <meta charset="utf-8">
-    <title>RNN (Red Neck NAS) Dashboard</title>
+    <title>RNN (RedNeck NAS) Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body class="bg-light">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="dashboard.php">RNN (Red Neck NAS)</a>
+            <a class="navbar-brand" href="dashboard.php">RNN (RedNeck NAS)</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
                 <span class="navbar-toggler-icon"></span>
             </button>
