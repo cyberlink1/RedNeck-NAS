387f2cf726e05b7d760aa0893e928dc06ddd4cc0
Local User <donotemailme@xganon.com>
2026-02-26 08:49:41 -0600

    Samba edit: only remove single preceding comment line

--- views/samba.php
+++ views/samba.php
@@ -88,13 +88,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     continue;
                 }
                 if (preg_match('/^\s*\[' . preg_quote($old, '/') . '\]\s*$/', $trim)) {
-                    // remove any comment lines immediately preceding the section header
-                    while (!empty($new)) {
+                    // drop a single comment line immediately above the header (if present)
+                    if (!empty($new)) {
                         $lastLine = $new[count($new)-1];
                         if (preg_match('/^\s*[#;]/', trim($lastLine))) {
                             array_pop($new);
-                        } else {
-                            break;
                         }
                     }
                     $skip = true;
